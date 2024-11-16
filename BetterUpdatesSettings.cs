838d9682390f3fffcd0d072b7af6ce40f0ef9665
Alex S. <salimalexo@gmail.com>
2024-11-16 14:32:32 +0100

    refactor: rename "tagName" to "completionName"

--- BetterUpdatesSettings.cs
+++ BetterUpdatesSettings.cs
@@ -10,11 +10,11 @@ namespace BetterUpdates
 {
     public class BetterUpdatesSettings : ObservableObject
     {
-        private string tagName = string.Empty;
+        private string completionName = string.Empty;
         private bool option2 = false;
         private bool optionThatWontBeSaved = false;
 
-        public string TagName { get => tagName; set => SetValue(ref tagName, value); }
+        public string TagName { get => completionName; set => SetValue(ref completionName, value); }
         public bool Option2 { get => option2; set => SetValue(ref option2, value); }
         // Playnite serializes settings object to a JSON object and saves it as text file.
         // If you want to exclude some property from being saved then use `JsonDontSerialize` ignore attribute.
